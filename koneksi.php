3549af9998fd9438f9ae796cf8f67235e071791a
Fathurrahman <38497343+nersus15@users.noreply.github.com>
2025-09-29 22:02:36 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
-$db_host = 'mysql';
-$db_user = 'dev';
-$db_pass = 'devpassword';
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
 $db_name = 'db_pertemuan4';
 
 $koneksi = new mysqli($db_host, $db_user, $db_pass, $db_name);
@@ -9,4 +9,5 @@ $koneksi = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($koneksi->connect_error) {
     die("Koneksi ke database gagal: " . $koneksi->connect_error);
 }
+
 ?>
